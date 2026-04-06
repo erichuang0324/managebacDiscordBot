a4834e3d0ec4822f2349d70a199b1409eaa03d2a
Eric Huag <erichuang0324@gmail.com>
2026-04-06 13:10:55 +0800

    Added User Info Command

--- discordBot.py
+++ discordBot.py
@@ -1,17 +1,17 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-
-
+from managebacAPI import managebacAPI
 class Client(commands.Bot):
     async def on_ready(self):
+        
         print(f'Logged on as {self.user}!')
         try:
             GUILD = discord.Object(id=1489832073383645244)
             synced = await self.tree.sync(guild=GUILD)
             print(f'Synced {len(synced)}')
         except Exception as e:
-            print(f'Error syncing Command') 
+            print(f'Error Syuncing: {e}') 
 
     async def on_message(self,message):
         if message.author == self.user:
@@ -27,12 +27,23 @@ client = Client(command_prefix="!",intents=intents)
 
 GUILD_ID = discord.Object(id=1489832073383645244)
 
-@client.tree.command(name="hello",description="Just Hello",guild=GUILD_ID)
-async def sayHello(interaction: discord.Interaction):
-    await interaction.response.send_message("Good Morning Yall")
 
-@client.tree.command(name="printer",description="Just Printing",guild=GUILD_ID)
-async def printer(interaction: discord.Interaction,printer: str):
-    await interaction.response.send_message(printer)
 
-client.run('<Authorization Token Goes Here>')
+
+@client.tree.command(name="auth", description="Tells you about your managebac Info", guild=GUILD_ID)
+async def auth(interaction: discord.Interaction,auth: str):
+
+    try:
+        api = managebacAPI(auth)
+        outList = api.user_info()
+
+        embed = discord.Embed(
+            title="Your Info",
+            description=f'Name:{outList[0]}\nYour Class is {outList[1]}\nYour School ID is {outList[2]}')
+        
+        await interaction.response.send_message(embed=embed)
+    except Exception as e:
+        print(e)
+
+
+client.run('Auth')
--- managebacAPI.py
+++ managebacAPI.py
@@ -2,51 +2,65 @@ import requests
 from bs4 import BeautifulSoup
 
 
-cookies = {
-    "_managebac_session": "%2BECHFGIAoepylNMWbYK6M0bqi5m6WkWRXRAeS8sAD4YqOq8G21Rd5a87Kjt2eCumkx3T%2BumaHrAeEEB6ab%2B0Zq6XQLvONey9FkdBpI5RjYPc540ubUMVNjWk2KUJvtFBXEbCp6okv2fLVNuDqsszQPtnVQpCEzrDWmAlSBl4bYpvtTa%2F4zA8Fdx0QydPSyjJzwemJ4Uy%2FdB6lwCKViVFWqdK3R0XpiunhMB4SxZPyglNssAbFB7S%2BXY9ewaV4MESip96n08leK4HJpjl8aashIjnNF69YM4YoETZT1Wq5ix2yvryCFAVXNuWvyKaIcrGYVts1085dnrcukJofB6GAxz%2BJfhp%2BntZwqI3cEspiEH9%2B2lKm1cIA3sgq0WUbYU%2FqsngrSqmHwQBX7yuTh9Xz7C5ej5A8s3NbmmZnRL%2Byuj8UogGBD33TF5Xsy7D3BcqZu%2FH9%2FMeZTMSLbnJ2ouea9%2B%2BkCnqwO2lQ5gOMAwSe6vM61DDzRfJdN%2FeM1rRhJ5c2tXBTiRj0PwYRHVoMGMNvVtiRI7G%2Bv7or%2F2YrwMTThShwwkUARnpnI2G1gDppHZxrt2Sdj5pLvxmzHyT93Fgt5OQCsKKd5b2j0ez3ZjBof9evuJsV8pJSHKF6dLnoAFfNO2rQ9ulNr4%3D--IvhezyToPg%2BLma8w--21gKQmV0Kg8HB%2BN67UUy6A%3D%3D"
-}
+class managebacAPI:
+
+    def __init__(self,session_id: str):
+        self.cookies ={
+            "_managebac_session" : session_id
+        }
+
+    def user_info(self) -> list:
+        r = requests.get("https://mingdao.managebac.com/student/profile", cookies=self.cookies)
+        soup = BeautifulSoup(r.text,'html.parser')
+
+        personalInfo = soup.find('div',class_="min-vw-0")
+        name = personalInfo.find('h1',class_='text-truncate f-page-header__text f-name-and-flag').get_text(strip=True)
+        className = personalInfo.find('a',class_='color-white').get_text(strip=True)
+        schoolID = personalInfo.find('span').get_text(strip=True)
+
+        return [name,className,schoolID]
+    def listAllClasses(self) -> dict:
+        r = requests.get("https://mingdao.managebac.com/student/classes/page/1", cookies=self.cookies)
+        soup = BeautifulSoup(r.text,'html.parser')
+
+        allListItems = soup.find_all('li',class_='f-menu__submenu-item')
+        class_data = {}
+        for listitem in allListItems:
+            div = listitem.find('div',class_='f-menu__submenu-icon f-menu__submenu-indicator')
+            if div:
+                class_id = ''.join(filter(str.isdigit,listitem.find('a',class_='f-menu__link f-menu__submenu-link').get('href')))
+                class_name = listitem.find('span',class_='f-menu__submenu-link-title').get_text()
+                class_data[class_name] = class_id
+        return class_data
+    
+    def listAllTaskesOfClass(self,class_id) -> dict:
+        # Maybe Incorprate findallclasses here
+        r = requests.get(f"https://mingdao.managebac.com/student/classes/{class_id}/core_tasks", cookies=self.cookies)
+        soup = BeautifulSoup(r.text,"html.parser")
+        allListItems = soup.find_all('div',class_="fusion-card-item short-assignment section hstack flex-wrap")
+        task_data ={}
+        for listItem in allListItems:
+            taskNameId = listItem.find('h4',class_='title')
+
+            task_name = taskNameId.find('a').get_text()
+            task_id = ''.join(filter(str.isdigit,taskNameId.find('a').get('href').split('/')[5]))
+            task_data[task_name] = task_id
+        return task_data
+    
+    def checkTaskScore(self,class_id,task_id):
+        r = requests.get(f"https://mingdao.managebac.com/student/classes/{class_id}/core_tasks", cookies=self.cookies)
+        soup = BeautifulSoup(r.text,"html.parser") 
+        allListItems = soup.find_all('div',class_="fusion-card-item short-assignment section hstack flex-wrap")
+        print(len(allListItems))
+        for listItem in allListItems:
+            task_id_get = listItem.find('a')
+            check_task_id = ''.join(filter(str.isdigit,task_id_get.get('href').split('/')[-1]))
+            if not task_id == check_task_id:
+                continue
+            assessmentSituation = listItem.select_one('.assessment.task-score')
+            print(assessmentSituation.get_text(strip=True))
 
 
-def listAllClasses() -> dict:
-    r = requests.get("https://mingdao.managebac.com/student/classes/page/1", cookies=cookies)
-    soup = BeautifulSoup(r.text,'html.parser')
-
-    allListItems = soup.find_all('li',class_='f-menu__submenu-item')
-    class_data = {}
-    for listitem in allListItems:
-        div = listitem.find('div',class_='f-menu__submenu-icon f-menu__submenu-indicator')
-        if div:
-            class_id = ''.join(filter(str.isdigit,listitem.find('a',class_='f-menu__link f-menu__submenu-link').get('href')))
-            class_name = listitem.find('span',class_='f-menu__submenu-link-title').get_text()
-            class_data[class_name] = class_id
-    return class_data
-
-def listAllTaskesOfClass(class_id) -> dict:
-    # Maybe Incorprate findallclasses here
-    r = requests.get(f"https://mingdao.managebac.com/student/classes/{class_id}/core_tasks", cookies=cookies)
-    soup = BeautifulSoup(r.text,"html.parser")
-    allListItems = soup.find_all('div',class_="fusion-card-item short-assignment section hstack flex-wrap")
-    task_data ={}
-    for listItem in allListItems:
-        taskNameId = listItem.find('h4',class_='title')
-
-        task_name = taskNameId.find('a').get_text()
-        task_id = ''.join(filter(str.isdigit,taskNameId.find('a').get('href').split('/')[5]))
-        task_data[task_name] = task_id
-    return task_data
-
-def checkTaskScore(class_id,task_id):
-    r = requests.get(f"https://mingdao.managebac.com/student/classes/{class_id}/core_tasks", cookies=cookies)
-    soup = BeautifulSoup(r.text,"html.parser") 
-    allListItems = soup.find_all('div',class_="fusion-card-item short-assignment section hstack flex-wrap")
-    print(len(allListItems))
-    for listItem in allListItems:
-        task_id_get = listItem.find('a')
-        check_task_id = ''.join(filter(str.isdigit,task_id_get.get('href').split('/')[-1]))
-        if not task_id == check_task_id:
-            continue
-        assessmentSituation = listItem.select_one('.assessment.task-score')
-        print(assessmentSituation.get_text(strip=True))
 
 
 
@@ -54,9 +68,14 @@ def checkTaskScore(class_id,task_id):
 
 
 
-# print(listAllClasses())
-# print(listAllTaskesOfClass(12781010))
-print(checkTaskScore(12781010,47581884))
+
+def main():
+    testAPI = managebacAPI("1ABiBqXEDjUPvXdGUS0g4N%2FXnDFtFbLBNSBBO0EeN1hsg7Z5YwGUfOk2osCL1mzVH33sKCKfvShDaawHs3PVs1CwutId52ltng9crQ0aRihpQcNfeMMp%2FXP3VGFJ0sFLr8qkQvFDmON0FRHDdsOzGFbseb3sTNYQVxMwsD%2Bo3A7KS9wMFwU8xHFtEePFkXzx8Y5EfyOpSCPJ5hHAQrGS7ccS%2FKkbXcHE0hPmA6EqdpNFPAdiaZz0gX2btZjxr9dYp7ZSGxghNYrDotgxLarLM8aLotq9FBLp81rH2HvH3JQgewONIw%2FH9gp07zqXGG%2BIkxWDpHgTCgXUj%2BjPdP7rT%2FmOhXhEziVVdycxw5x0ipXKb4U7TN7WQToNX%2F92d6BRn4XrFfuSeqRVu%2BjqKJbKVTASCgTLq0WJ6J70l%2BRcXs8bvl5Avg4Nw0fCLo2X8l%2FYDs3uQTTkkhcIrlBkfAWFnn8sUEWPNBUuCUrmJuwh4LLGBySUcG3ndHu%2BCbzvMD4ghstDcg5dLL4tqqRo%2BKCtMq7OioeDm22Kah1IYKINvKqMD3EbpZ%2BIqQexX0pJpG1q08DTOX1%2FKwFHctCsPMHqI67eRija6BgQEKiX6SZNvi0wXZQqRS8%2F5wN5CcjUSd1IHTAUuNGM9u4%3D--H6TNx4rCQcbROKK%2B--C1zUxmnstijdiPKBEbaW%2Bg%3D%3D")
+    print(testAPI.user_info())
+
+if __name__ == "__main__":
+    main()
+
 
 
 
