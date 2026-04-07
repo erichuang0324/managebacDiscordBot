4de570c1b407fcbc8515e7932d11427559636136
Eric Huag <erichuang0324@gmail.com>
2026-04-07 20:12:51 +0800

    Fixed enf added cc button

--- discordBot.py
+++ discordBot.py
@@ -51,17 +51,17 @@ async def auth(interaction: discord.Interaction,auth: str):
         print(e)
 
 class View(discord.ui.View):
-    @discord.ui.button(label="Click me LMAO", style=discord.ButtonStyle.gray)
+    @discord.ui.button(label="Spawn Chloe Chua", style=discord.ButtonStyle.gray,emoji="😊")
     async def button_callback(self,button,interaction):
         await button.response.send_message("https://media.discordapp.net/attachments/1278908628488949781/1487312702886903808/chloe.gif?ex=69d48ce5&is=69d33b65&hm=bb51289088763aa863bb3d9e42bf84699ec26410eb695ec378e26ca25a4182c2&=&width=1424&height=1216")
         
 
-@client.tree.command(name="hmmbutton",description="Cool emoji dispencer", guild=GUILD_ID)
+@client.tree.command(name="chloebutton",description="Just Chloe", guild=GUILD_ID)
 async def chloebutton(interaction: discord.Interaction):
     await interaction.response.send_message(view=View())
     
 @client.tree.command(name="checktaskscore",description="Check Task Score", guild=GUILD_ID)
-async def checkTaskScore(interaction: discord.Interaction,test,test2):
-    await interaction.response.send_message(test,test2)
-    
+async def checkTaskScore(interaction: discord.Interaction,test: str,test2: str):
+    await interaction.response.send_message(f'{test},{test2}')
+
 client.run(os.getenv("DISCORD_BOT_API"))
